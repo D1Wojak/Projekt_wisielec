600122e3a574df3a217e0157efcb01b664c4e224
Patryk <pat.stan.lew@gmail.com>
2022-09-09 20:41:32 +0200

    na funkjcach

--- main.py
+++ main.py
@@ -2,65 +2,84 @@ from random import randint
 import codecs
 import sys
 
-#Podanie imienia
+def wybierzPoziom ():
+    lvl = int(input())
+    while lvl < 1 or lvl > 4:
+        print("Nie wybrałeś żadnego poziomu trudności! Spróbuj ponownie poziomy 1-4.")
+        lvl = int(input())
+    return lvl
 
+def wczytywanieHasel (lvl):
+    nazwapliku = "hasla" + str(poziom) + ".txt"
+    hasla = []
+    with codecs.open(nazwapliku, 'r', 'utf-8') as plik_z_haslami:
+        for line in plik_z_haslami:
+            hasla.append(str(line))
+    return hasla
+def ukrywanieHasla(haslo =[]):
+    for i in range(len(haslo)):
+        if tablica[i] == " ":
+            tablica[i] = " "
+        else:
+            tablica[i] = "_"
+    return tablica
+
+
+def gra(tablica, tablica1):
+    zycia = 0
+    while zycia < 9:
+       print(nick + " pozostało Ci :"+ str(9-zycia) + " zyc.")
+       print(" ".join(tablica))
+
+       print("Podaj JEDNA litere " + str(nick) + " :")
+       litera = input()
+       litera = litera.lower()
+       if len(litera) > 1:
+           print("Popelniłeś błąd! Podaj WYŁĄCZNIE JEDNĄ litere! Następny błąd będzie skutkował utratą życia!")
+           litera = input()
+           litera = litera.lower()
+
+       if litera in haslo:
+           for i in range(len(haslo)):
+               if haslo[i] == litera:
+                   tablica[i] = litera
+           if tablica == haslo:
+               print(nick, "Brawo, wygrałeś!")
+               break
+
+       else:
+           zycia +=1
+           print(grafika[zycia-1])
+           if zycia == 9:
+               print("Przegrałeś " + nick)
+               print("Poprawna odpowiedź to: ")
+               print("".join(tablica1))
+
+
+
+
+
+
+### Podanie imienia ###
 print("Podaj imię: ")
 nick = input()
 
-### Wybór poziomu trudności
-
+### Wybór poziomu trudności ###
 print("Wybierz poziom trudności : ")
 print("1 - Łatwy (wyrazy do 5 liter) ")
 print("2 - Średni (wyrazy do 10 liter) ")
 print("3 - Trudny (wyrazy do 15 liter) ")
 print("4 - Niemożliwy (15 i więcej liter) ")
-poziom = input()
-poziom = int(poziom)
+poziom = wybierzPoziom()
 
 ### Wczytywanie haseł z pliku ###
-if poziom <1 or poziom >4:
-    print("Nie wybrałeś żadnego poziomu trudności! Następny błąd będzie skutkował wyłączeniem się programu!")
-    poziom = input()
-    poziom = int(poziom)
-if poziom == 1:
-    plik_z_haslami = codecs.open("hasla1.txt", 'r', 'utf-8')
-if poziom == 2:
-    plik_z_haslami = codecs.open("hasla2.txt", 'r', 'utf-8')
-if poziom == 3:
-    plik_z_haslami = codecs.open("hasla3.txt", 'r', 'utf-8')
-if poziom == 4:
-    plik_z_haslami = codecs.open("hasla4.txt", 'r', 'utf-8')
-
-haslaa = plik_z_haslami.read()
-plik_z_haslami.close()
-
-### Licznik haseł ###
-hasla = str(haslaa.lower())
-licznik = 0
-for i in hasla:
-    if  i == ".":
-        licznik += 1
-
+hasla = wczytywanieHasel(poziom)
 
 ### Losowanie hasła ###
-nr_wylosowanego_hasla = randint(1, licznik)
+nr_wylosowanego_hasla = randint(1, len(hasla))
 
 ### Wczytywanie wylosownaego hasła ###
-haslo = []
-licznik1 = 0
-
-for i in hasla:
-    if i == ".":
-        licznik1 +=1
-    if licznik1 == nr_wylosowanego_hasla:
-        haslo += i
-haslo.remove(".")
-for i in haslo:
-    if i == "\n":
-        haslo.remove("\n")
-for i in haslo:
-    if i == "\r":
-        haslo.remove("\r")
+haslo = hasla[nr_wylosowanego_hasla-1]
 
 ### Wisielec ###
 grafika = [ """
@@ -129,38 +148,5 @@ grafika = [ """
 #     _____         #"""]
 tablica = list(haslo)
 tablica1= list(haslo)
-zycia = 0
-for i in range(len(haslo)):
-    if tablica[i] == " ":
-        tablica[i]=" "
-    else:
-        tablica[i] = "_"
-
-
-while zycia < 9:
-    print(nick + " pozostało Ci :"+ str(9-zycia) + " zyc.")
-    print(" ".join(tablica))
-
-    print("Podaj JEDNA litere " + str(nick) + " :")
-    litera = input()
-    litera = litera.lower()
-    if len(litera) > 1:
-        print("Popelniłeś błąd! Podaj WYŁĄCZNIE JEDNĄ litere! Następny błąd będzie skutkował utratą życia!")
-        litera = input()
-        litera = litera.lower()
-
-    if litera in haslo:
-        for i in range(len(haslo)):
-            if haslo[i] == litera:
-                tablica[i] = litera
-        if tablica == haslo:
-            print(nick, "Brawo, wygrałeś!")
-            break
-
-    else:
-        zycia +=1
-        print(grafika[zycia-1])
-        if zycia == 9:
-            print("Przegrałeś " + nick)
-            print("Poprawna odpowiedź to: ")
-            print("".join(tablica1))
+tablica = ukrywanieHasla(tablica)
+gra(tablica, tablica1)
